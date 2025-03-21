c4ca518ee394d19e8537015f2c38638224b10f84
Sean Schnell <seansps@gmail.com>
2025-03-21 17:09:21 -0400

    Add automation for shield damage in damageScript

--- rollhandlers/damageScript.js
+++ rollhandlers/damageScript.js
@@ -3,6 +3,12 @@
 let damage = value;
 let coverDamaged = false;
 let coverDestroyed = false;
+
+let shieldDamaged = false;
+let shieldDestroyed = false;
+
+const valuesToSet = {};
+
 // Ignore negative damage as that is what healing is for
 if (damage > 0) {
   const oldCoverHp = parseInt(record.data?.coverHp || "0", 10);
@@ -11,12 +17,26 @@ if (damage > 0) {
   // In Cyberpunk Red, damage to cover doesn't affect the character
   // Only apply damage to character if there was no cover to begin with
   if (newCoverHp !== oldCoverHp) {
-    api.setValue("data.coverHp", newCoverHp);
+    valuesToSet["data.coverHp"] = newCoverHp;
     // Cover took the damage, so we don't damage the character
     damage = 0;
     coverDamaged = true;
     if (newCoverHp === 0) {
       coverDestroyed = true;
+    }
+  }
+
+  // If damage is greater than 0, check shield HP next if shield is up
+  const shieldUp = record.data?.shieldDown === "shieldUp";
+  const shieldHp = parseInt(record.data?.shieldHP || "0", 10);
+  if (damage > 0 && shieldHp > 0 && shieldUp) {
+    const oldShieldHp = shieldHp;
+    const newShieldHp = Math.max(oldShieldHp - damage, 0);
+    valuesToSet["data.shieldHP"] = newShieldHp;
+    shieldDamaged = true;
+    damage = 0;
+    if (newShieldHp === 0) {
+      shieldDestroyed = true;
     }
   }
 
@@ -28,7 +48,9 @@ if (damage > 0) {
   if (curhp > record.data?.hitpoints) {
     curhp = record.data?.hitpoints;
   }
-  api.setValue("data.curhp", curhp);
+  valuesToSet["data.curhp"] = curhp;
+
+  api.setValues(valuesToSet);
 }
 
 // If damage > 0, float text
@@ -42,6 +64,12 @@ if (value > 0 && token) {
       api.floatText(token, `Cover Destroyed`, "#0000FF");
     } else {
       api.floatText(token, `Cover Damaged by ${value}`, "#0000FF");
+    }
+  } else if (shieldDamaged) {
+    if (shieldDestroyed) {
+      api.floatText(token, `Shield Destroyed`, "#0000FF");
+    } else {
+      api.floatText(token, `Shield Damaged by ${value}`, "#0000FF");
     }
   } else {
     api.floatText(token, `-${value}`, "#FF0000");
